97ee64a1743b70df89b40bdcb1c64d237e2a685c
Billy Arredondo <billyarredondo.coder@gmail.com>
2025-07-21 02:58:42 +0200

    Add Identity and JWT authentication with EF Core
    Introduced AppDbContext using IdentityDbContext and configured Entity Framework Core with PostgreSQL. Added JWT authentication and Identity endpoints, updated appsettings for database connection, and included necessary NuGet packages. Removed the .http file and adjusted nullable settings in the project file.

--- gubiarpa.kidso-challenge.webapi/Program.cs
+++ gubiarpa.kidso-challenge.webapi/Program.cs
@@ -1,11 +1,56 @@
-var builder = WebApplication.CreateBuilder(args);
+using gubiarpa.kidso_challenge.webapi.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
 
-// Add services to the container.
+string CreateSecretKey()
+{
+    var secretBytes = new Byte[64];
+    using var random = RandomNumberGenerator.Create();
+    random.GetBytes(secretBytes);
+    var secretKey = Convert.ToBase64String(secretBytes);
+    return secretKey;
+}
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+    option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "FreeTrained",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(CreateSecretKey()))
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>(options =>
+    {
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders(); ;
 
 var app = builder.Build();
 
@@ -15,6 +60,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
 
